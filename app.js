8743146c4a58d809347375b22833b79f85f9f651
Alexander Hedling <taikes_bass@hotmail.com>
2022-05-30 14:05:56 +0200

    uppdaterat ordlista + helmet

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require("dotenv").config()
 const { PORT } = process.env
 const Express = require("express")
 const app = Express()
+const helmet = require("helmet")
 
+app.use(helmet())
 app.use(Express.static("public"))
 
 app.listen(PORT, () => {
